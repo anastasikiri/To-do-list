5d8bff54eb081b996ed7bb56813f5a63069dafa0
Anastasia Bilous <anastasitokar@gmail.com>
2022-09-08 23:23:11 -0400

    project created

---
+++ To-do list/Controllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-do list
+//
+//  Created by Kyrylo Tokar on 2022-09-08.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ To-do list/Controllers/TaskDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaskDetailsViewController.swift
+//  To-do list
+//
+//  Created by Kyrylo Tokar on 2022-09-08.
+//
+
+import UIKit
+
+class TaskDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
