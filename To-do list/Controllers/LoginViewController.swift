5bedc69d860b2c0752a3275e533e11ee0b65b376
anastasikiri <anastasitokar@gmail.com>
2022-09-12 12:18:22 -0400

    all sceens logic updated

--- To-do list/Controllers/LoginViewController.swift
+++ To-do list/Controllers/LoginViewController.swift
@@ -8,8 +8,9 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    var credentials = [Credentials]()
+    
+    var users = [User]()
+    var userIndex = Int()
     
     
     @IBOutlet weak var loginTextField: UITextField!
@@ -18,19 +19,94 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        users = UserDefaultsManager.shared.getValueForUser() ?? [User]()
+        print(users)
     }
-
-
+    
     @IBAction func signInButton(_ sender: UIButton) {
         
+        if  loginTextField.text?.isEmpty == true {
+            Alert.showBasic(
+                title: "Please enter your login",
+                vc: self)
+        } else if passwordTextField.text?.isEmpty == true {
+            Alert.showBasic(
+                title: "Please enter your password",
+                vc: self)
+        } else if users.isEmpty  {
+            Alert.showBasic(
+                title: "Invalid login, please register",
+                vc: self)
+        } else {
+            for i in users.indices {
+                if users[i].login == loginTextField.text, users[i].password == passwordTextField.text {
+                    userIndex = i
+                    let taskListVC = self.storyboard?.instantiateViewController(
+                        withIdentifier: "TaskListViewController") as? TaskListViewController
+                    taskListVC?.userIndex = userIndex
+                    self.navigationController?.pushViewController(taskListVC!, animated: true)
+                    
+                    loginTextField.text = ""
+                    passwordTextField.text = ""
+                    break
+                    
+                } else if users[i].login == loginTextField.text, users[i].password != passwordTextField.text {
+                    Alert.showBasic(
+                        title: "Invalid password",
+                        vc: self)
+                } else if users[i].login != loginTextField.text, i == users.count-1 {
+                    Alert.showBasic(
+                        title: "Invalid login, please register or correct login",
+                        vc: self)
+                }
+            }
+        }
     }
     
     @IBAction func registerButton(_ sender: UIButton) {
-        credentials.append(
-            Credentials(login: loginTextField.text ?? "", password: passwordTextField.text ?? "")
-        )
-        print(credentials)
+        if  loginTextField.text?.isEmpty == true {
+            Alert.showBasic(
+                title: "Please enter your login",
+                vc: self)
+        } else if passwordTextField.text?.isEmpty == true {
+            Alert.showBasic(
+                title: "Please enter your password",
+                vc: self)
+        } else if users.isEmpty == true {
+            users.append(User(login: loginTextField.text ?? "",
+                              password: passwordTextField.text ?? ""))
+            UserDefaultsManager.shared.setValueForUser(value: users)
+            
+            print(users)
+            loginTextField.text = ""
+            passwordTextField.text = ""
+            
+            Alert.showBasic(
+                title: "Successfully registered, please Sign in",
+                vc: self)
+        } else {
+            users = UserDefaultsManager.shared.getValueForUser() ?? [User]()
+            
+            if users.contains(where: {$0.login == loginTextField.text}) {
+                Alert.showBasic(
+                    title: "User already exist, please Sign in",
+                    vc: self)
+                loginTextField.text = ""
+                passwordTextField.text = ""
+            } else {
+                users.append(User(login: loginTextField.text ?? "",
+                                  password: passwordTextField.text ?? ""))
+                UserDefaultsManager.shared.setValueForUser(value: users)
+                
+                print(users)
+                loginTextField.text = ""
+                passwordTextField.text = ""
+                
+                Alert.showBasic(
+                    title: "Successfully registered, please Sign in",
+                    vc: self)
+            }
+        }
     }
-    
 }
 
--- To-do list/Controllers/TaskListViewController.swift
+++ To-do list/Controllers/TaskListViewController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 
-class TaskListViewController: UIViewController {
-
+class TaskListViewController: UIViewController, TaskTableViewCellDelegate {
+    
+    var userIndex = Int()
+    var users = [User]()
+    var tasks = [Task]()
     
     @IBOutlet weak var tasksTableView: UITableView!
     
@@ -19,28 +22,112 @@ class TaskListViewController: UIViewController {
         tasksTableView.delegate = self
         tasksTableView.dataSource = self
     }
-   
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        users = UserDefaultsManager.shared.getValueForUser() ?? [User]()
+        tasks = users[userIndex].task ?? [Task]()
+        tasksTableView.reloadData()
+    }
+    
+    @IBAction func addButton(_ sender: UIButton) {
+        let taskDetailsVC = self.storyboard?.instantiateViewController(
+            withIdentifier: "TaskDetailsViewController") as? TaskDetailsViewController
+        taskDetailsVC?.userIndex = userIndex
+        taskDetailsVC?.choosenTableViewCell = false
+        self.navigationController?.pushViewController(taskDetailsVC!, animated: true)
+    }
+    
+    func didTapStatusButton(cell: TaskTableViewCell) {
+        if let chosenIndex = tasksTableView.indexPath(for: cell) {
+            var currentStatus = tasks[chosenIndex.row].status
+            
+            
+            switch currentStatus {
+            case "in progress":
+                currentStatus = "done"
+                print("current status: \(currentStatus)")
+                tasks[chosenIndex.row].status = currentStatus
+                tasksTableView.reloadData()
+                users[userIndex].task?[chosenIndex.row].status = currentStatus
+                UserDefaultsManager.shared.setValueForUser(value: users)
+            case "done":
+                currentStatus = "to do"
+                print("current status: \(currentStatus)")
+                tasks[chosenIndex.row].status = currentStatus
+                tasksTableView.reloadData()
+                users[userIndex].task?[chosenIndex.row].status = currentStatus
+                UserDefaultsManager.shared.setValueForUser(value: users)
+            case "to do":
+                currentStatus = "in progress"
+                print("current status: \(currentStatus)")
+                tasks[chosenIndex.row].status = currentStatus
+                tasksTableView.reloadData()
+                users[userIndex].task?[chosenIndex.row].status = currentStatus
+                UserDefaultsManager.shared.setValueForUser(value: users)
+            default:
+                break
+            }
+        }
+    }
 }
 
 extension TaskListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tasksTableView.deselectRow(at: indexPath, animated: true)
+        let taskDetailsVC = self.storyboard?.instantiateViewController(
+            withIdentifier: "TaskDetailsViewController") as? TaskDetailsViewController
+        taskDetailsVC?.userIndex = userIndex
+        taskDetailsVC?.taskIndex = indexPath.row
+        taskDetailsVC?.choosenTableViewCell = true
+        self.navigationController?.pushViewController(taskDetailsVC!, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   commit editingStyle: UITableViewCell.EditingStyle,
+                   forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tableView.beginUpdates()
+            users[userIndex].task?.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            UserDefaultsManager.shared.setValueForUser(value: users)
+            tableView.endUpdates()
+        }
     }
 }
 
 extension TaskListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        //        return users[userIndex].task?.count ?? 0
+        return tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell",for: indexPath) as? TaskTableViewCell
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: "TaskTableViewCell",for: indexPath) as? TaskTableViewCell
         else {
             fatalError()
         }
+        tasks = tasks.sorted(by: { $0.status > $1.status })
+        users[userIndex].task = tasks
+        UserDefaultsManager.shared.setValueForUser(value: users)
+        cell.titleLabel.text = tasks[indexPath.row].title
+        cell.descriptionLabel.text = tasks[indexPath.row].description
+        cell.deadlineLabel.text = tasks[indexPath.row].deadline
+        cell.statusButtonOutlet.setTitle(tasks[indexPath.row].status, for: .normal)
+        
+        switch tasks[indexPath.row].status {
+        case "in progress":
+            cell.statusButtonOutlet.backgroundColor = .systemOrange
+        case "done":
+            cell.statusButtonOutlet.backgroundColor = .systemRed
+        case "to do":
+            cell.statusButtonOutlet.backgroundColor = .systemGreen
+        default:
+            break
+        }
+        cell.delegate = self
         return cell
     }
-    
-    
 }
 
